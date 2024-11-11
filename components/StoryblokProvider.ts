25626a973fc104b64c596a3122a4a6836dd03915
TNDKien <kienn770@gmail.com>
2024-11-11 15:30:22 +0100

    first commit

---
+++ components/StoryblokProvider.ts
@@ -0,0 +1,34 @@
+"use client";
+
+import {
+  storyblokInit,
+  apiPlugin,
+  SbReactComponentsMap,
+} from "@storyblok/react/rsc";
+import Page from "./page.component";
+import Feature from "./Feature/Feature";
+import ThreeQuarterLayout from "./ThreeQuarterLayout/ThreeQuarterLayout";
+import Template from "./Template/Template";
+
+const components = {
+  page: Page,
+  feature: Feature,
+  threequarterlayout: ThreeQuarterLayout,
+  template: Template,
+};
+
+storyblokInit({
+  accessToken: process.env.NEXT_PUBLIC_STORYBLOK_ACCESS_TOKEN,
+  use: [apiPlugin],
+  components: components as SbReactComponentsMap,
+});
+
+export default function StoryblokProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+}
+
+console.log(process.env.NEXT_PUBLIC_STORYBLOK_ACCESS_TOKEN);
