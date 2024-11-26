d1612b1efb332f3c7d54114f2afef8a66b262d40
TNDKien <kienn770@gmail.com>
2024-11-26 10:44:55 +0100

    richtext update + article layout

--- app/artikelen/[slug]/page.tsx
+++ app/artikelen/[slug]/page.tsx
@@ -1,11 +1,13 @@
-import { getStoryblokApi } from "@storyblok/react"; // Adjust this import based on your setup
-import fetchData from "@utils/fetch-data";
+import { getStoryblokApi } from "@storyblok/react";
+import { render } from "storyblok-rich-text-react-renderer";
 
 export async function getData(slug: string) {
   const storyblokApi = getStoryblokApi();
   const response = await storyblokApi.get(`cdn/stories/artikelen/${slug}`, {
-    version: "draft", // Use "published" in production
+    version: "draft",
+    cv: Date.now(),
   });
+  console.log("Storyblok response:", response.data.story);
   return response.data.story;
 }
 
@@ -15,14 +17,19 @@ export default async function ArticlePage({
   params: { slug: string };
 }) {
   const { slug } = params;
-  const story = await getData(slug); // Fetch the article data from Storyblok
-  const { content } = story; // Content fields (e.g., Titel, Afbeelding, Teaser, Content)
+  const story = await getData(slug);
+  const blok = story.content;
 
   return (
-    <article className="flex flex-col justify-center text-center">
-      <h1 className="flex text-3xl max-w-[612px]">{content.titel}</h1>
-      <img src={content.Afbeelding.filename} alt={content.Afbeelding.alt} />
-      <p>{content.teaser}</p>
+    <article className="flex flex-col justify-center items-center min-h-screen p-4">
+      <header className="container mx-auto m-8 max-w-[612px] text-center">
+        <h1 className="text-3xl mb-6">{blok.titel}</h1>
+        <p className="mb-12">{blok.teaser}</p>
+      </header>
+      <img src={blok.afbeelding.filename} alt={blok.afbeelding.alt} />
+      <section className="prose max-w-[612px] mt-8">
+        {render(blok.content)}
+      </section>
     </article>
   );
 }
