f9607cf9e763890e0fddc95ec057a430ae8b9728
TNDKien <kienn770@gmail.com>
2024-11-29 14:06:16 +0100

    update styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,17 +20,17 @@ module.exports = {
         standaard: ["Montserrat", "sans-serif"],
       },
       fontSize: {
-        "h1-desktop": ["32px", "auto"], // Page Title (Desktop)
-        "h2-desktop": ["24px", "32px"], // Headline (Desktop)
-        "p-desktop": ["20px", "24px"], // Page Description / Paragraph (Desktop)
-        "h3-desktop": ["20px", "auto"], // Small Headline (Desktop)
-        "label-desktop": ["14px", "16px"], // Label (Desktop)
-        "alt-desktop": ["12px", "auto"], // Alternative (Desktop)
-        "h1-mobile": ["24px", "28px"], // Page Title (Mobile)
-        "h2-mobile": ["20px", "24px"], // Headline (Mobile)
-        "p-mobile": ["16px", "24px"], // Paragraph / Small Headline (Mobile)
-        "label-mobile": ["14px", "16px"], // Author / Info (Mobile)
-        "alt-mobile": ["12px", "auto"], // Label/Alternative (Mobile)
+        "h1-desktop": ["32px", { lineHeight: "auto", fontWeight: "700" }], // Page Title (Desktop)
+        "h2-desktop": ["24px", { lineHeight: "32px", fontWeight: "600" }], // Headline (Desktop)
+        "p-desktop": ["20px", { lineHeight: "24px", fontWeight: "400" }], // Page Description / Paragraph (Desktop)
+        "h3-desktop": ["20px", { lineHeight: "auto", fontWeight: "500" }], // Small Headline (Desktop)
+        "label-desktop": ["14px", { lineHeight: "16px", fontWeight: "400" }], // Label (Desktop)
+        "alt-desktop": ["12px", { lineHeight: "auto", fontWeight: "400" }], // Alternative (Desktop)
+        "h1-mobile": ["24px", { lineHeight: "28px", fontWeight: "700" }], // Page Title (Mobile)
+        "h2-mobile": ["20px", { lineHeight: "24px", fontWeight: "600" }], // Headline (Mobile)
+        "p-mobile": ["16px", { lineHeight: "24px", fontWeight: "400" }], // Paragraph / Small Headline (Mobile)
+        "label-mobile": ["14px", { lineHeight: "16px", fontWeight: "400" }], // Author / Info (Mobile)
+        "alt-mobile": ["12px", { lineHeight: "auto", fontWeight: "400" }], // Label/Alternative (Mobile)
       },
       animation: {},
       keyframes: {},
