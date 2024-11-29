6348aee4b991c596112caacb9a1b1bbbdbb10beb
TNDKien <kienn770@gmail.com>
2024-11-29 14:05:58 +0100

    tailwind config setup
    quick setup from basic design system figma to config with an AI

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,19 @@ module.exports = {
         paragraaf: ["IBM Plex Serif", "serif"],
         standaard: ["Montserrat", "sans-serif"],
       },
+      fontSize: {
+        "h1-desktop": ["32px", "auto"], // Page Title (Desktop)
+        "h2-desktop": ["24px", "32px"], // Headline (Desktop)
+        "p-desktop": ["20px", "24px"], // Page Description / Paragraph (Desktop)
+        "h3-desktop": ["20px", "auto"], // Small Headline (Desktop)
+        "label-desktop": ["14px", "16px"], // Label (Desktop)
+        "alt-desktop": ["12px", "auto"], // Alternative (Desktop)
+        "h1-mobile": ["24px", "28px"], // Page Title (Mobile)
+        "h2-mobile": ["20px", "24px"], // Headline (Mobile)
+        "p-mobile": ["16px", "24px"], // Paragraph / Small Headline (Mobile)
+        "label-mobile": ["14px", "16px"], // Author / Info (Mobile)
+        "alt-mobile": ["12px", "auto"], // Label/Alternative (Mobile)
+      },
       animation: {},
       keyframes: {},
     },
