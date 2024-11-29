4b0c8a90e7ee844c9292276623adb5ae8081be42
hanlyvt <hanlytrang@gmail.com>
2024-11-29 11:25:56 +0100

    Header container aanmaak

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,14 +1,20 @@
 import { storyblokEditable } from "@storyblok/react/rsc";
 
-const Sidebar = ({ blok }) => (
+const Header = ({ blok }) => (
   <div className=" bg-blue-500 text-white" {...storyblokEditable(blok)}>
-    <h1 className=" text-4xl font-bold">{blok.Titel}</h1>
-    <img
-      className="w-1/2"
-      src={blok.Afbeelding.filename}
-      alt={blok.Afbeelding.alt}
-    />
+    {/* <h1 className=" text-4xl font-bold">{blok.Titel}</h1> */}
+
+    <div className="bg-blue-600 h-28">
+      .
+      <a href="/">
+        <img
+          className="w-48 h-12 ml-24"
+          src={blok.logo.filename}
+          alt={blok.logo.alt || "Logo"}
+        />
+      </a>
+    </div>
   </div>
 );
 
-export default Sidebar;
+export default Header;
