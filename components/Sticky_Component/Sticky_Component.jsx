4d1e0c31f7310e1bad53c6e42736bfcaa5fd0415
Marijn Snoeren <m.snoeren@student.fontys.nl>
2024-12-02 15:22:20 +0100

    sticky v1

--- components/Sticky_Component/Sticky_Component.jsx
+++ components/Sticky_Component/Sticky_Component.jsx
@@ -1,13 +1,26 @@
 import { storyblokEditable } from "@storyblok/react/rsc";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter, faWhatsapp, faFacebookF, faLinkedinIn, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 const Sticky_Component = ({ blok }) => (
-  <div className=" bg-blue-500 text-white" {...storyblokEditable(blok)}>
-    <h1 className=" text-4xl font-bold">{blok.Titel}</h1>
-    <img
-      className="w-1/2"
-      src={blok.Afbeelding.filename}
-      alt={blok.Afbeelding.alt}
-    />
+  <div className="" {...storyblokEditable(blok)}>
+        <div className="p-10">
+          <a href="https://twitter.com" className="my-4 flex items-center justify-center w-10 h-10 bg-red text-white rounded-full">
+            <FontAwesomeIcon icon={faTwitter} size="L" />
+          </a>
+          <a href="https://facebook.com" className="my-4 flex items-center justify-center w-10 h-10 bg-red text-white rounded-full">
+            <FontAwesomeIcon icon={faFacebookF} size="L" />
+          </a>
+          <a href="https://linkedin.com" className="my-4 flex items-center justify-center w-10 h-10 bg-red text-white rounded-full">
+            <FontAwesomeIcon icon={faLinkedinIn} size="L" />
+          </a>
+          <a href="https://instagram.com" className="my-4 flex items-center justify-center w-10 h-10 bg-red text-white rounded-full">
+            <FontAwesomeIcon icon={faInstagram} size="L" />
+          </a>
+          <a href="https://whatsapp.com" className="my-4 flex items-center justify-center w-10 h-10 bg-red text-white rounded-full">
+            <FontAwesomeIcon icon={faWhatsapp} size="L" />
+          </a>
+        </div>
   </div>
 );
 
