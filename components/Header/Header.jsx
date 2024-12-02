88550f29e79aaaefe540c774d52d624c3c4fa4a3
hanlyvt <hanlytrang@gmail.com>
2024-12-02 10:49:28 +0100

    eerste list uitklap menu

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -3,8 +3,7 @@ import "./HoverEffect.css";
 
 const Header = ({ blok }) => (
   <div className="-28 mt-0" {...storyblokEditable(blok)}>
-    {/* <h1 className=" text-4xl font-bold">{blok.Titel}</h1> */}
-
+    {/* nav bar */}
     <nav className="flex ml-24 mr-24 justify-between pb-8">
       <div className="w-3/12">
         <a href="/">
@@ -27,6 +26,7 @@ const Header = ({ blok }) => (
         <li className="flex">
           <a className="flex justify-center items-center" href="">
             Nieuws
+            {/* SVG voor pijl die naar beneden wijst */}
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -67,6 +67,7 @@ const Header = ({ blok }) => (
         </li>
       </ul>
 
+      {/* Zoek afbeelding */}
       <div className="w-3/12">
         <div className="flex justify-end">
           <img
@@ -75,20 +76,47 @@ const Header = ({ blok }) => (
             alt={blok.zoek.alt || "Zoek"}
           />
 
+          {/* Aboneer knop */}
           <button className="text-white bg-rood px-4 py-1 mt-12 ml-6 font-semibold">
             Aboneer
           </button>
 
+          {/* Kleurmodus knop */}
           <img
             className="mt-12 ml-6"
             src={blok.modus.filename}
-            alt={blok.modus.alt || "Zoek"}
+            alt={blok.modus.alt || "Kleurmodus knop"}
           />
         </div>
       </div>
     </nav>
 
-    <div className="h-72 bg-zwart">.</div>
+    {/* Uitklapmenu */}
+    <div className="h-72 bg-zwart">
+      <div className="ml-24 mr-24 flex flex-row justify-between">
+        <ul>
+          <li className="mt-12 ">
+            <a className="text-rood">Innovaties</a>
+          </li>
+          <li className="">
+            <a className="text-wit">AI</a>
+          </li>
+          <li className="">
+            <a className="text-wit">AR & VR</a>
+          </li>
+          <li className="">
+            <a className="text-wit">Tech Doorbraken</a>
+          </li>
+          <li className="">
+            <a className="text-wit">Mobiele Innovaties</a>
+          </li>
+        </ul>
+        <div className="bg-rood">5 elementen</div>
+        <div className="bg-rood">5 elementen</div>
+        <div className="bg-rood">5 elementen</div>
+        <div className="bg-rood">5 elementen</div>
+      </div>
+    </div>
   </div>
 );
 
