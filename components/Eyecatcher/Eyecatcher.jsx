89664461e486cda66d7003e09fe2a13ad9d176b1
Marijn Snoeren <m.snoeren@student.fontys.nl>
2024-12-02 11:02:11 +0100

    eyecatcher mobile goede account

--- components/Eyecatcher/Eyecatcher.jsx
+++ components/Eyecatcher/Eyecatcher.jsx
@@ -9,7 +9,7 @@ const Eyecatcher = ({ blok }) => (
     <img
       src={blok.afbeelding.filename}
       alt={blok.afbeelding.alt}
-      className="w-full h-auto rounded-t-md object-cover"
+      className="min-w-full h-[240px] rounded-t-md object-cover mx-auto"
     />
 
     {/* Titel */}
@@ -22,12 +22,12 @@ const Eyecatcher = ({ blok }) => (
     </h1>
 
     {/* Body */}
-    <p className="mt-4 pr-8 text-zwart font-paragraaf leading-relaxed">
+    <p className="mt-4 pr-8 text-zwart font-paragraaf leading-relaxed line-clamp-5 sm:line-clamp-none">
       {blok.body}
     </p>
 
     {/* Auteur, tijd en Call-to-action */}
-    <div className="mt-4 flex items-center justify-between text-sm text-zwart">
+    <div className="mt-4 flex items-center justify-between text-sm text-zwart hidden sm:flex">
       {/* Auteur en tijd */}
       <p>
         <span className="font-semibold">{blok.auteur}</span> • {blok.tijd}
@@ -42,3 +42,5 @@ const Eyecatcher = ({ blok }) => (
 );
 
 export default Eyecatcher;
+
+//m.snoeren@student.fontys.nl
