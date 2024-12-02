12178700b5d91bffc9519ca6cf01bc30d8578423
Marijn Snoeren <m.snoeren@student.fontys.nl>
2024-12-02 15:37:35 +0100

    sticky v2

--- components/Sticky_Component/Sticky_Component.jsx
+++ components/Sticky_Component/Sticky_Component.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
 import { storyblokEditable } from "@storyblok/react/rsc";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTwitter, faWhatsapp, faFacebookF, faLinkedinIn, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
-const Sticky_Component = ({ blok }) => (
-  <div className="" {...storyblokEditable(blok)}>
+const Sticky_Component = ({ blok }) => {
+  // State voor de scrollindicator
+  const [scrollPercent, setScrollPercent] = useState(0);
+
+  useEffect(() => {
+    // Functie die de scrollpositie bijhoudt
+    const handleScroll = () => {
+      const scrollTop = window.scrollY; // Hoe ver we gescrold zijn
+      const docHeight = document.documentElement.scrollHeight - window.innerHeight; // Totale hoogte van de pagina
+      const scrollPosition = (scrollTop / docHeight) * 100; // Percentage van scrollen
+      setScrollPercent(scrollPosition); // Stel het percentage in
+    };
+
+    // Voeg een event listener toe voor scroll
+    window.addEventListener("scroll", handleScroll);
+
+    // Verwijder de event listener bij unmounten
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+  <div className="sticky top-0" {...storyblokEditable(blok)}>
         <div className="p-10">
           <a href="https://twitter.com" className="my-4 flex items-center justify-center w-10 h-10 bg-red text-white rounded-full">
             <FontAwesomeIcon icon={faTwitter} size="L" />
@@ -20,8 +43,15 @@ const Sticky_Component = ({ blok }) => (
           <a href="https://whatsapp.com" className="my-4 flex items-center justify-center w-10 h-10 bg-red text-white rounded-full">
             <FontAwesomeIcon icon={faWhatsapp} size="L" />
           </a>
+          <div
+          className="w-10 h-10 bg-white rounded-full"
+          style={{
+            background: `conic-gradient(#E94560 ${scrollPercent}%, #e5e7eb ${scrollPercent}%)`,
+          }}
+        />
         </div>
   </div>
 );
+};
 
 export default Sticky_Component;
