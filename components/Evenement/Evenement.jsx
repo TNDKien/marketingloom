0f0da3672bc055a902e1be64e1b93d60dde55775
TNDKien <kienn770@gmail.com>
2024-12-03 17:52:08 +0100

    event info/intro

--- components/Evenement/Evenement.jsx
+++ components/Evenement/Evenement.jsx
@@ -1,15 +1,16 @@
 import { storyblokEditable } from "@storyblok/react/rsc";
+import { EventInfo } from "../EventInfo/EventInfo";
 
 const Evenement = ({ blok }) => {
   return (
     <article
-      className="relative w-full px-24 flex justify-center items-center"
+      className="flex flex-col relative w-full px-24 justify-center items-center"
       {...storyblokEditable(blok)}
     >
       {/* Banner */}
-      <div className="relative w-full h-96 flex overflow-hidden">
+      <section className="relative w-full h-96 flex overflow-hidden">
         {/* Background Image */}
-        <div className="absolute right-0 top-0 w-3/5 h-full z-0">
+        <div className="absolute right-0 top-0 w-3/4 h-full z-0">
           <img
             src={blok.afbeelding.filename}
             alt={blok.afbeelding.alt || "Background Image"}
@@ -18,7 +19,7 @@ const Evenement = ({ blok }) => {
         </div>
 
         {/* Red Banner and Content */}
-        <div className="absolute left-0 top-0 w-3/4 h-full z-10 flex items-center">
+        <div className="absolute left-0 top-0 w-8/12 h-full z-10 flex items-center">
           {/* SVG Banner */}
           <img
             src="/Banner.svg"
@@ -60,7 +61,23 @@ const Evenement = ({ blok }) => {
             {/* Button */}
           </div>
         </div>
-      </div>
+      </section>
+
+      {/* Event Info */}
+      <section className="flex ">
+        <div className=" flex flex-col items-start justify-between w-1/2 py-8 pr-6">
+          <div className="flex flex-col gap-4">
+            <h3 className="text-h2-desktop">{blok.intro_kopje}</h3>
+            <p>{blok.intro_tekst}</p>
+          </div>
+          <button>hoi</button>
+        </div>
+        <img
+          className="w-1/2 py-6"
+          src={blok.intro_afbeelding.filename}
+          alt={blok.intro_afbeelding.alt}
+        />
+      </section>
     </article>
   );
 };
