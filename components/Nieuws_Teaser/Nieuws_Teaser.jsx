8b0f5c201bff33d88e26c650e1bd1e57da273437
Marijn Snoeren <m.snoeren@student.fontys.nl>
2024-12-05 10:10:03 +0100

    nieuwe button

--- components/Nieuws_Teaser/Nieuws_Teaser.jsx
+++ components/Nieuws_Teaser/Nieuws_Teaser.jsx
@@ -18,7 +18,7 @@ const Nieuws_Teaser = ({ blok }) => (
           {/* Mobiele versie: Auteur en tijd op dezelfde lijn */}
           <div className="flex pt-0 text-red font-normal md:hidden justify-between items-center pt-2 font-semibold text-label-desktop text-black">
             <span>{blok.auteur}</span>
-            <span className="text-silver">{blok.tijd}</span>
+            <span className="text-silver font-normal">{blok.tijd}</span>
           </div>
 
           {/* Titel */}
@@ -36,9 +36,13 @@ const Nieuws_Teaser = ({ blok }) => (
             <span className="flex flex-col md:flex-row gap-1 pt-2 font-semibold">
               {blok.auteur} <span className="text-silver font-normal">{blok.tijd}</span>
             </span>
-            <a href="#" className="pt-2 md:pt-3 font-semibold hover:underline">
-              Lees meer →
-            </a>
+        <button className="group relative flex items-center text-sm font-semibold text-black">
+          <span className="relative z-10">Lees Meer</span>
+          <span className="ml-2 transition-transform duration-300 ease-out group-hover:-translate-x-2 opacity-100 group-hover:opacity-0">
+            →
+          </span>
+          <span className="absolute bottom-0 left-0 w-0 h-[1px] bg-black transition-all duration-300 group-hover:w-full"></span>
+        </button>
           </div>
         </div>
       </div>
