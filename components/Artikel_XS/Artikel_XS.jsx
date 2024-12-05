7e2e5844d5a1f60aa2654b211ee79452e424ee5e
koenderaden <koenderaden@gmail.com>
2024-12-05 10:54:34 +0100

    mobile background dark Artikel_XS

--- components/Artikel_XS/Artikel_XS.jsx
+++ components/Artikel_XS/Artikel_XS.jsx
@@ -1,34 +1,38 @@
 import { storyblokEditable } from "@storyblok/react";
 
 const Artikel_XS = ({ blok }) => (
-  <div
-    className="bg-white text-black p-4 mb-4 max-w-[360px] min-h-[110px] flex flex-row group cursor-pointer"
-    {...storyblokEditable(blok)}
-    onClick={() => {
-      window.location.href = blok["artikel_xs_link"]; // Verwijst naar de gewenste URL
-    }}
-  >
-    {/* Afbeelding */}
-    <img
-      className="h-[110px] w-[110px] object-cover transition-opacity duration-300 group-hover:opacity-80"
-      src={blok["artikel_xs_image"]?.filename}
-      alt={blok["artikel_xs_image"]?.alt || "Afbeelding"}
-    />
+  <div className="p-4 bg-black sm:bg-white" {...storyblokEditable(blok)}>
 
-    {/* Tekstcontainer */}
-    <div className="ml-4 flex flex-col justify-between max-w-235">
-      {/* Titel */}
-      <h1 className="text-label-desktop font-ser text-red mb-1 group-hover:underline">
-        {blok["artikel_xs_title"]}
-      </h1>
-      {/* Paragraaf */}
-      <p className="text-label-desktop text-black max-w-235 group-hover:underline">
-        {blok["artikel_xs_paragraph"]}
-      </p>
-      {/* Label */}
-      <span className="text-alt-desktop text-silver max-w-235 font-medium bg-white group-hover:underline">
-        {blok["artikel_xs_label"]}
-      </span>
+    {/* Artikel */}
+
+    <div
+      className="text-white mb-4 flex flex-row group cursor-pointer sm:bg-white sm:text-black sm:max-w-[360px] sm:min-h-[110px]"
+      onClick={() => {
+        window.location.href = blok["artikel_xs_link"]; // Verwijst naar de gewenste URL
+      }}
+    >
+      {/* Afbeelding */}
+      <img
+        className="h-[110px] w-[110px] object-cover transition-opacity duration-300 group-hover:opacity-80 mr-4"
+        src={blok["artikel_xs_image"]?.filename}
+        alt={blok["artikel_xs_image"]?.alt || "Afbeelding"}
+      />
+
+      {/* Tekstcontainer */}
+      <div className="flex flex-col justify-between max-w-full">
+        {/* Titel */}
+        <h1 className="text-label-desktop font-ser font-semibold text-red mb-1 group-hover:underline">
+          {blok["artikel_xs_title"]}
+        </h1>
+        {/* Paragraaf */}
+        <p className="text-label-desktop group-hover:underline">
+          {blok["artikel_xs_paragraph"]}
+        </p>
+        {/* Label */}
+        <span className="text-alt-desktop text-silver font-medium group-hover:underline">
+          {blok["artikel_xs_label"]}
+        </span>
+      </div>
     </div>
   </div>
 );
