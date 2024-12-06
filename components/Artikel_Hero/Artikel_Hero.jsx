a2100d72f966ee9a10a19ae16cc3be504dcbc15c
hanlyvt <hanlytrang@gmail.com>
2024-12-06 11:13:28 +0100

    search blok statisch

--- components/Artikel_Hero/Artikel_Hero.jsx
+++ components/Artikel_Hero/Artikel_Hero.jsx
@@ -8,14 +8,30 @@ const Artikel_Hero = ({ blok }) => (
     <div className="text-p-large-desktop text-center mt-8">
       {blok.inleiding}
     </div>
+
+    {/* Searchbar */}
+    <div className="mt-14 flex justify-end">
+      <div className="w-1/4 flex justify-between border-red border-2 h-12 items-center ">
+        <span className="ml-6 text-label-desktop font-semibold">
+          Zoek interview..
+        </span>
+        <img
+          className="mr-6"
+          src={blok.search.filename}
+          alt={blok.search.alt || "search"}
+        />
+      </div>
+    </div>
+
     <div className="flex mt-6">
       <div>
         <img
           className=""
           src={blok.artikel_afbeelding.filename}
-          alt={blok.artikel_afbeelding.alt || "Logo"}
+          alt={blok.artikel_afbeelding.alt || "Speaker"}
         />
       </div>
+
       <div className="ml-8">
         <div className="w-fit text-label-desktop font-regular text-white bg-red px-4 py-1 ">
           {blok.categorie}
