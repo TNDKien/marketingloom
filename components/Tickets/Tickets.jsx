f5cb00ca6b811bebc307765ec6f72d382bdd19ca
koenderaden <koenderaden@gmail.com>
2024-12-11 10:57:38 +0100

    Responsive design tickets

--- components/Tickets/Tickets.jsx
+++ components/Tickets/Tickets.jsx
@@ -4,58 +4,57 @@ const Tickets = ({ blok }) => {
   return (
     <div
       {...storyblokEditable(blok)}
-      className="flex flex-row gap-12 p-6 px-24"
+      className="flex flex-col lg:flex-row gap-12 p-6 lg:px-24"
     >
       {/* Tickets Section */}
-      <div className="flex flex-col gap-6 w-1/2">
-        <h2 className="text-3xl font-bold text-red">Tickets</h2>
+      <div className="flex flex-col gap-6 w-full lg:w-1/2">
+        <h2 className="text-2xl font-bold text-red">Tickets</h2>{" "}
+        {/* Kleiner formaat */}
         {/* Ticket 1 */}
         <div className="flex flex-row w-full gap-4 border-b-2 border-black items-center py-4">
           {/* Naam */}
-          <p className="text-lg font-bold text-black flex-1">Early Bird</p>
+          <p className="text-base font-bold text-black flex-1">Early Bird</p>
 
           {/* Prijs */}
-          <p className="text-lg text-black flex-1 text-center">€ 295,-</p>
+          <p className="text-base text-black flex-1 text-center">€ 295,-</p>
 
           {/* Knop */}
           <button className="bg-red text-white w-[106px] h-[34px] rounded hover:bg-silver">
-            <h1>Bestel Nu</h1>
+            <h1 className="text-sm font-bold">Bestel Nu</h1>
           </button>
         </div>
-
         {/* Ticket 2 */}
         <div className="flex flex-row w-full gap-4 border-b-2 border-black items-center py-4">
           {/* Naam */}
-          <p className="text-lg font-bold text-black flex-1">Regulier</p>
+          <p className="text-base font-bold text-black flex-1">Regulier</p>
 
           {/* Prijs */}
-          <p className="text-lg text-black flex-1 text-center">€ 345,-</p>
+          <p className="text-base text-black flex-1 text-center">€ 345,-</p>
 
           {/* Knop */}
           <button className="bg-red text-white w-[106px] h-[34px] rounded hover:bg-silver">
-            <h1>Bestel Nu</h1>
+            <h1 className="text-sm font-bold">Bestel Nu</h1>
           </button>
         </div>
-
         {/* Ticket 3 */}
         <div className="flex flex-row w-full gap-4 border-b-2 border-black items-center py-4">
           {/* Naam */}
-          <p className="text-lg font-bold text-black flex-1">Groepskorting</p>
+          <p className="text-base font-bold text-black flex-1">Groepskorting</p>
 
           {/* Prijs */}
-          <p className="text-lg text-black flex-1 text-center">
+          <p className="text-base text-black flex-1 text-center">
             tot 15% korting
           </p>
 
           {/* Knop */}
           <button className="bg-red text-white w-[106px] h-[34px] rounded hover:bg-silver">
-            <h1>Bestel Nu</h1>
+            <h1 className="text-sm font-bold">Bestel Nu</h1>
           </button>
         </div>
       </div>
 
       {/* Partner worden Section */}
-      <div className="flex flex-col gap-4 w-1/2">
+      <div className="flex flex-col gap-4 w-full lg:w-1/2">
         <h2 className="text-3xl font-bold text-black">Partner worden?</h2>
         <p className="text-lg text-black">
           Als partner van The Marketing Loom, onze toonaangevende
